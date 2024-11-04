47cbbc71d915de2cec9d2693d5882a01dbb0f0ad
Sannya Wasim <sannya.wasim01@gmail.com>
2024-11-04 02:19:05 -0800

    Setup React and Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='text-4xl text-red font-play mx-auto'>Bytewave Tech</p>
+    </div>
+  );
+}
+
+export default App;
